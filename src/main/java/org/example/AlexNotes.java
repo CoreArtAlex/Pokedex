66384bf249f7806423d728f22912357fea67b8b3
alejandroguerravillegas <coreartalex2@gmail.com>
2022-04-04 07:24:20 -0700

    Created a Class with notes of my pending & downloaded the full project & created my branch

---
+++ src/main/java/org/example/AlexNotes.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class AlexNotes {
+
+    // Find a Tutorial on how to change the visuals and colors of the elements on Jvfx
+    // Create a class to control the calling of stats, moves & About
+    // with a method for each different menu ... hopefully it works
+
+}
