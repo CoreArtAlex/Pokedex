ac69db0442d0b562e4e52efa66cf1bdc27920939
alejandroguerravillegas <coreartalex2@gmail.com>
2022-04-05 14:01:37 -0700

    Add tables to the about, stats & move sections

--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -3,13 +3,18 @@ package org.example;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class SecondaryController {
 
     public Button secondaryButton;
+    public ListView<String> about = new ListView<>();
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+
+
 }
