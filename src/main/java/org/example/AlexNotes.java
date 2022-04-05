2b470f1cc858f78ad6b7a4d0537d74791481b4b1
alejandroguerravillegas <coreartalex2@gmail.com>
2022-04-05 08:11:36 -0700

    Changing UI

--- src/main/java/org/example/AlexNotes.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class AlexNotes {
-
-    // Find a Tutorial on how to change the visuals and colors of the elements on Jvfx
-    // Create a class to control the calling of stats, moves & About
-    // with a method for each different menu ... hopefully it works
-
-}
